30d8fb8841a9470027b16e611bd1fb1ea7d13760
Emmanuel Ezenwigbo <44367769+SkyC0der@users.noreply.github.com>
2020-05-11 12:46:39 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const gifts = {
+    0: 'day of Christmas my true love gave to me:',
+    1: [' first ', ' a Partridge in a Pear Tree.\n'],
+    2: [' second ', ' two Turtle Doves, and'],
+    3: [' third ', ' three French Hens,'],
+    4: [' fourth ', ' four Calling Birds,'],
+    5: [' fifth ', ' five Gold Rings,'],
+    6: [' sixth ', ' six Geese-a-Laying,'],
+    7: [' seventh ', ' seven Swans-a-Swimming,'],
+    8: [' eighth ', ' eight Maids-a-Milking,'],
+    9: [' ninth ', ' nine Ladies Dancing,'],
+    10: [' tenth ', ' ten Lords-a-Leaping,'],
+    11: [' eleventh ', ' eleven Pipers Piping,'],
+    12: [' twelfth ', ' twelve Drummers Drumming,']
+  }
+  
+  export default class TwelveDays {
+    //type code here
+  }
+
+  module.exports = TwelveDays;
